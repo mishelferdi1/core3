ba1f6163d40bfb8630751a1573b7b37cdcf51132
mishelferdi1 <101936449@student.swin.edu.au>
2020-10-29 16:36:50 +1100

    core3

---
+++ app/src/main/java/com/example/mytown2/mytown2/Location.kt
@@ -0,0 +1,10 @@
+package com.example.mytown2.mytown2
+
+data class Location(
+    val locationName: String,
+    val timezone: String,
+    val longitude: Float,
+    val latitude: Float,
+    val favorite: Boolean
+)
+
